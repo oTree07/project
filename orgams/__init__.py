8bb6ba655ed92d14a6515b72226c6f9b21ebf5cf
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2023-07-10 11:58:07 -0500

    update structure

--- orgams/__init__.py
+++ orgams/__init__.py
@@ -9,8 +9,10 @@ class C(BaseConstants):
     NAME_IN_URL = 'organ_donation'
     PLAYERS_PER_GROUP = None
     NUM_ROUNDS = 20
-    CHANGE_COST = 0.75
+    CHANGE_COST = cu(0.75)
 
+class Subsession(BaseSubsession):
+    pass
 
 class Group(BaseGroup):
     donaciones = models.IntegerField(initial=0)
@@ -28,9 +30,51 @@ class Player(BasePlayer):
     fuera_de_juego = models.BooleanField(initial=False)
 
 
-class Subsession(BaseSubsession):
-    pass
+# FUNCTIONS
 
+def ElegirDonar(p: Player):
+    g = p.group
+    if p.es_donante:
+        g.donaciones += 1
+        if p.round_number <= 10:
+            p.payoff -= C.CHANGE_COST
+
+
+def SimularCaso(p: Player):
+    g = p.group
+    caso = random.choices(['A', 'B', 'C'], [0.10, 0.20, 0.70])[0]
+    p.caso = caso
+
+    if caso == 'A':
+        p.organo_a_funcional = False
+        p.fuera_de_juego = True
+    elif caso == 'B':
+        if p.donacion_previa:
+            p.fuera_de_juego = True
+        else:
+            p.organo_b_funcional = False
+            p.en_lista_espera = True
+    else:
+        #p.organo_a_funcional = True
+        #p.organo_b_funcional = True
+        p.payoff += 3
+
+
+def EvaluarLista(p: Player):
+    g = p.group
+
+    if g.donaciones > 0:
+        g.donaciones -= 1
+        p.donacion_previa = True
+        p.periodos_en_lista = 0
+
+    if p.periodos_en_lista >= 5:
+        p.fuera_de_juego = True
+    
+    p.periodos_en_lista += 1
+
+
+# PAGES
 
 class Donacion(Page):
     timeout_seconds = 15
@@ -38,78 +82,48 @@ class Donacion(Page):
     form_fields = ['es_donante']
 
     @staticmethod
-    def is_displayed(player: Player):
-        return player.round_number == 1
+    def is_displayed(p: Player):
+        return p.round_number == 1
 
     @staticmethod
-    def before_next_page(player: Player, timeout_happened):
-        group = player.group
-        if player.es_donante:
-            group.donaciones += 1
-            if player.round_number <= 10:
-                player.payoff -= C.CHANGE_COST
-
-
-class Espera(WaitPage):
-    pass
+    def before_next_page(p: Player, timeout_happened):
+        ElegirDonar(p)
 
 
 class Simulacion(Page):
     timeout_seconds = 5
 
     @staticmethod
-    def before_next_page(player: Player, timeout_happened):
-        group = player.group
-        caso = random.choices(['A', 'B', 'C'], [0.10, 0.20, 0.70])[0]
-        player.caso = caso
-
-        if caso == 'A':
-            player.organo_a_funcional = False
-            player.fuera_de_juego = True
-        elif caso == 'B':
-            if player.donacion_previa:
-                player.fuera_de_juego = True
-            else:
-                player.organo_b_funcional = False
-                player.en_lista_espera = True
-        else:
-            #player.organo_a_funcional = True
-            #player.organo_b_funcional = True
-            player.payoff += 3
-
+    def is_displayed(p: Player):
+        return not p.en_lista_espera and not p.fuera_de_juego
+    
     @staticmethod
-    def is_displayed(player: Player):
-        return not player.en_lista_espera and not player.fuera_de_juego
+    def before_next_page(p: Player, timeout_happened):
+        SimularCaso(p)
 
 
 class ListaEspera(Page):
     timeout_seconds = 5
 
     @staticmethod
-    def before_next_page(player: Player, timeout_happened):
-        group = player.group
-
-        if group.donaciones > 0:
-            group.donaciones -= 1
-            player.donacion_previa = True
-            player.periodos_en_lista = 0
-
-        if player.periodos_en_lista >= 5:
-            player.fuera_de_juego = True
-        
-        player.periodos_en_lista += 1
-
+    def is_displayed(p: Player):
+        return p.en_lista_espera and not p.fuera_de_juego
+    
     @staticmethod
-    def is_displayed(player: Player):
-        return player.en_lista_espera and not player.fuera_de_juego
+    def before_next_page(p: Player, timeout_happened):
+        EvaluarLista(p)
+
+
+class Espera(WaitPage):
+    pass
 
 
 class FinRonda(Page):
     timeout_seconds = 5
 
     @staticmethod
-    def is_displayed(player: Player):
-        return player.fuera_de_juego
+    def is_displayed(p: Player):
+        return p.fuera_de_juego
 
 
 page_sequence = [
