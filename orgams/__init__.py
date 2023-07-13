0366b97d8f4ec8d8429e8186b70a98bf29790b4c
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2023-07-12 22:46:05 -0500

    safe data

--- orgams/__init__.py
+++ orgams/__init__.py
@@ -9,18 +9,26 @@ class C(BaseConstants):
     NAME_IN_URL = 'organ_donation'
     PLAYERS_PER_GROUP = None
     NUM_ROUNDS = 20
-    CHANGE_COST = cu(0.75)
+    CHANGE_COST = 0.75
 
 class Subsession(BaseSubsession):
     pass
+    #def creating_session(self):
+    #    self.group_randomly(fixed_id_in_group=True)
+    #    self.group_by_arrival_time()
 
 class Group(BaseGroup):
     donaciones = models.IntegerField(initial=0)
 
+    def before_session_starts(self):
+        for player in self.get_players():
+            player.donacion_previa = False
+            player.en_lista_espera = False
+            player.fuera_de_juego = False
+            player.pago = 0.0
 
 class Player(BasePlayer):
     caso = models.CharField(initial="")
-    round_number = models.IntegerField(initial=0)
     periodos_en_lista = models.IntegerField(initial=0)
     organo_a_funcional = models.BooleanField(initial=True)
     organo_b_funcional = models.BooleanField(initial=True)
@@ -28,23 +36,39 @@ class Player(BasePlayer):
     donacion_previa = models.BooleanField(initial=False)
     en_lista_espera = models.BooleanField(initial=False)
     fuera_de_juego = models.BooleanField(initial=False)
+    pago = models.FloatField(initial=0.0)
 
 
 # FUNCTIONS
+
+def guardar_valores_jugadores(subsession):
+    if subsession.round_number > 1:
+        for player in subsession.get_players():
+            jugador_anterior = player.in_round(subsession.round_number - 1)
+            #player.jugador_anterior_id = jugador_anterior.id_in_group
+            player.caso = jugador_anterior.caso
+            player.periodos_en_lista = jugador_anterior.periodos_en_lista
+            player.organo_a_funcional = jugador_anterior.organo_a_funcional
+            player.organo_b_funcional = jugador_anterior.organo_b_funcional
+            player.es_donante = jugador_anterior.es_donante
+            player.donacion_previa = jugador_anterior.donacion_previa
+            player.en_lista_espera = jugador_anterior.en_lista_espera
+            player.fuera_de_juego = jugador_anterior.fuera_de_juego
+            player.pago = jugador_anterior.pago
+
 
 def log(p: Player, fx):
     g = p.group
 
     print("Player {}: {} | Donante: {} | Donaciones: {} | Donacion Previa: {} | Caso: {} | Organo A: {} | Organo B: {} | Lista de Espera: {} | Periodos: {} | Fuera: {} | Payoff: {}".format(
-        p.id_in_group, fx, p.es_donante, g.donaciones, p.donacion_previa, p.caso, p.organo_a_funcional, p.organo_b_funcional, p.en_lista_espera, p.periodos_en_lista, p.fuera_de_juego, p.payoff))
+        p.id_in_group, fx, p.es_donante, g.donaciones, p.donacion_previa, p.caso, p.organo_a_funcional, p.organo_b_funcional, p.en_lista_espera, p.periodos_en_lista, p.fuera_de_juego, p.pago))
     
 
 def ElegirDonar(p: Player):
     g = p.group
     if p.es_donante:
-        g.donaciones += 1
         if p.round_number <= 10:
-            p.payoff -= C.CHANGE_COST
+            p.pago -= C.CHANGE_COST
     
     log(p, "ElegirDonar")
 
@@ -57,6 +81,8 @@ def SimularCaso(p: Player):
     if caso == 'A':
         p.organo_a_funcional = False
         p.fuera_de_juego = True
+        if p.es_donante:
+            g.donaciones += 1
     elif caso == 'B':
         if p.donacion_previa:
             p.fuera_de_juego = True
@@ -66,7 +92,7 @@ def SimularCaso(p: Player):
     else:
         #p.organo_a_funcional = True
         #p.organo_b_funcional = True
-        p.payoff += 3
+        p.pago += 3.0
     
     log(p, "SimularCaso")
 
@@ -96,6 +122,7 @@ class Donacion(Page):
 
     @staticmethod
     def is_displayed(p: Player):
+        guardar_valores_jugadores(p.subsession)
         return p.round_number == 1
 
     @staticmethod
@@ -132,8 +159,6 @@ class Espera(WaitPage):
 
 
 class FinRonda(WaitPage):
-    #after_all_players_arrive = True
-
     @staticmethod
     def is_displayed(p: Player):
         return p.fuera_de_juego
