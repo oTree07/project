cfc2863bac664093dc7e8e363192e7eae21745c9
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2023-07-15 12:14:58 -0500

    fix initial -round

--- orgams/__init__.py
+++ orgams/__init__.py
@@ -4,6 +4,9 @@ from otree.api import *
 doc = """
 Juego de donación de órganos
 """
+INICIO = True
+TURNO = 1
+RONDA = 1
 
 class C(BaseConstants):
     NAME_IN_URL = 'organ_donation'
@@ -16,7 +19,8 @@ class Subsession(BaseSubsession):
 
 
 class Group(BaseGroup):
-    ronda = models.IntegerField(initial=0)
+    ronda = models.IntegerField(initial=1)
+    turno = models.IntegerField(initial=1)
     donaciones = models.IntegerField(initial=0)
 
 
@@ -29,8 +33,8 @@ class Player(BasePlayer):
     donacion_previa = models.BooleanField(initial=False)
     en_lista_espera = models.BooleanField(initial=False)
     fuera_de_juego = models.BooleanField(initial=False)
+    fin_turno = models.BooleanField(initial=False)
     pago = models.FloatField(initial=0.0)
-    iteracion = models.IntegerField(initial=0)
 
 
 # FUNCTIONS
@@ -38,8 +42,8 @@ class Player(BasePlayer):
 def log(p: Player, fx):
     g = p.group
 
-    print("Player {}: {} | Donante: {} | Donaciones: {} | Donacion Previa: {} | Caso: {} | Organo A: {} | Organo B: {} | Lista de Espera: {} | Periodos: {} | Fuera: {} | Payoff: {}".format(
-        p.id_in_group, fx, p.es_donante, g.donaciones, p.donacion_previa, p.caso, p.organo_a_funcional, p.organo_b_funcional, p.en_lista_espera, p.periodos_en_lista, p.fuera_de_juego, p.pago))
+    print("{} - Player {}: {} | Donante: {} | Donaciones: {} | Donacion Previa: {} | Caso: {} | Organo A: {} | Organo B: {} | Lista de Espera: {} | Periodos: {} | Fuera: {} | Payoff: {}".format(
+        g.turno, p.id_in_group, fx, p.es_donante, g.donaciones, p.donacion_previa, p.caso, p.organo_a_funcional, p.organo_b_funcional, p.en_lista_espera, p.periodos_en_lista, p.fuera_de_juego, p.pago))
     
 def ResetearJugador(p: Player):
     subsession = p.subsession
@@ -55,35 +59,80 @@ def ResetearJugador(p: Player):
         p.en_lista_espera = False
         p.fuera_de_juego = False
         p.pago = 0.0
-        p.iteracion = 0
+        g.turno = 1
     
     g.donaciones = 0 
 
 def GuardarJugador(p: Player):
     subsession = p.subsession
     g = p.group
-
-    if subsession.round_number > 1:
+    print("guar")
+    if TURNO > 1:
+        print("I: " + str(g.turno))
         for p in subsession.get_players():
-            jugador_anterior = p.in_round(subsession.round_number - 1)
-            g.donaciones = jugador_anterior.group.donaciones
-            g.ronda = jugador_anterior.group.ronda
-            p.caso = jugador_anterior.caso
-            p.periodos_en_lista = jugador_anterior.periodos_en_lista
-            p.organo_a_funcional = jugador_anterior.organo_a_funcional
-            p.organo_b_funcional = jugador_anterior.organo_b_funcional
-            p.es_donante = jugador_anterior.es_donante
-            p.donacion_previa = jugador_anterior.donacion_previa
-            p.en_lista_espera = jugador_anterior.en_lista_espera
-            p.fuera_de_juego = jugador_anterior.fuera_de_juego
-            p.pago = jugador_anterior.pago
+            print(str(TURNO) + "www" + str(g.turno))
+            anterior = p.in_round(p.round_number - 1)
+            g.donaciones = anterior.group.donaciones
+            g.turno = anterior.group.turno
+            g.ronda = anterior.group.ronda
+            p.caso = anterior.caso
+            p.periodos_en_lista = anterior.periodos_en_lista
+            p.organo_a_funcional = anterior.organo_a_funcional
+            p.organo_b_funcional = anterior.organo_b_funcional
+            p.es_donante = anterior.es_donante
+            p.donacion_previa = anterior.donacion_previa
+            p.en_lista_espera = anterior.en_lista_espera
+            p.fuera_de_juego = anterior.fuera_de_juego
+            p.pago = anterior.pago
 
+
+def Donar():
+    global INICIO
+    global TURNO
+
+    if TURNO == 1:
+        return True
+    else:
+        return False
+
+def FIN():
+    global RONDA
+
+    return RONDA
+    
+def Evaluar(p: Player):
+    global INICIO
+    global TURNO
+    global RONDA
+
+    #INICIO = False
+    
+    g = p.group
+    p.fin_turno = True
+
+    if all(p.fin_turno == True for p in g.subsession.get_players()):
+        print("I: " + str(g.turno))
+        
+        if INICIO == False:
+            g.turno += 1
+            TURNO += 1
+
+        INICIO = False
+
+    if all(p.fuera_de_juego == True for p in g.subsession.get_players()):
+        print("FIN RONDA " + str(g.ronda))
+        ResetearJugador(p)
+        g.ronda += 1
+        INICIO = True
+        TURNO = 1
+        RONDA += 1
 
 def ElegirDonar(p: Player):
+
     g = p.group
 
     if p.es_donante:
-        if p.round_number <= 10:
+        if g.ronda <= 10:
             p.pago -= C.CHANGE_COST
     
     log(p, "ElegirDonar")
@@ -139,8 +188,14 @@ class Donacion(Page):
 
     @staticmethod
     def is_displayed(p: Player):
-        GuardarJugador(p)
-        return p.round_number == 1
+        global INICIO
+
+        print("DOn" + str(Donar()))
+
+        if not Donar():
+            GuardarJugador(p)
+
+        return Donar()
 
     @staticmethod
     def before_next_page(p: Player, timeout_happened):
@@ -152,7 +207,7 @@ class Simulacion(Page):
 
     @staticmethod
     def is_displayed(p: Player):
-        return not p.en_lista_espera and not p.fuera_de_juego
+        return not p.en_lista_espera and not p.fuera_de_juego 
     
     @staticmethod
     def before_next_page(p: Player, timeout_happened):
@@ -177,24 +232,19 @@ class Espera(WaitPage):
         return not p.fuera_de_juego
 
 
-class FinRonda(WaitPage):
-    template_name = 'orgams/FinRonda.html'
+class FinTurno(WaitPage):
+    template_name = 'orgams/FinTurno.html'
 
     @staticmethod
     def is_displayed(p: Player):
         return p.fuera_de_juego
 
-class Resultados(Page):
+class FinRonda(Page):
     @staticmethod
     def is_displayed(p: Player):
-        g= p.group
-
-        if all(p.fuera_de_juego == True for p in g.subsession.get_players()):
-            print("FIN RONDA " + str(g.ronda))
-            ResetearJugador(p)
-            g.ronda += 1
-
-        return g.ronda == 2
+        print("FIN")
+        Evaluar(p)
+        return FIN() == 3
 
     @staticmethod
     def app_after_this_page(p: Player, upcoming_apps):
@@ -205,6 +255,7 @@ page_sequence = [
     Espera,
     Simulacion,
     ListaEspera,
+    FinTurno,
     FinRonda,
-    Resultados
+    Espera
 ]
